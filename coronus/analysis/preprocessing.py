e398578118ea7c546b51c0e75bc6ea2de87b6553
Marian Andrecki <marian.andrecki@gmail.com>
2020-03-11 23:40:21 +0000

    moved functionalities outside

---
+++ coronus/analysis/preprocessing.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+
+def cases_to_growths(df_active, return_log=False):
+    filtered = df_active.copy()
+    filtered[filtered<20] = np.nan
+    log_gr = np.log(filtered).reset_index().diff(1).replace([np.inf, -np.inf], np.nan)
+    log_gr = log_gr.dropna(axis="columns", how="all")
+    new_cols = {}
+    for col in log_gr.iloc[:, 1:].columns:
+        col_vals = log_gr.loc[log_gr[col].idxmax() - 3:, col].reset_index(drop=True)
+        new_cols.update({col: col_vals})
+    shifted_log_gr = pd.DataFrame(new_cols)
+    shifted_log_gr.index = shifted_log_gr.index - 3
+    shifted_log_gr.index.name = "days since peak growth"
+    if return_log:
+        return shifted_log_gr
+    else:
+        return np.exp(shifted_log_gr)
---
+++ coronus/plotting/plots.py
@@ -0,0 +1,18 @@
+import plotly.express as px
+
+
+def plot_interactive_df(df, ylabel, legend_label, name_sort=False):
+    if name_sort:
+        order = sorted(df.columns)
+    else:
+        order = df.max().sort_values(ascending=False).index.tolist()
+
+    df_plot = df[order].reset_index()
+    df_plot = df_plot.melt(id_vars=[df.index.name], value_name=ylabel, var_name=legend_label)
+    fig = px.line(
+        data_frame=df_plot,
+        x=df.index.name,
+        y=ylabel,
+        color=legend_label,
+    )
+    return fig
