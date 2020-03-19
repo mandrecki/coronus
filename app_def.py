b272e691472b40e51062ffaf1b0a488ae5d152de
Bartek Marcinowski <nethopp3r@gmail.com>
2020-03-19 20:35:05 +0100

    Add Google Analytics.
    Kto wie, chyba dziala.

--- app_def.py
+++ app_def.py
@@ -2,6 +2,6 @@
 import dash
 
 external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css', 'https://fonts.googleapis.com/css?family=Nova+Slim|Raleway&display=swap']
-dash_app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+dash_app = dash.Dash(__name__, external_stylesheets=external_stylesheets, external_scripts=[{'src': 'https://www.googletagmanager.com/gtag/js?id=UA-161352689-1', 'async': True}])
 dash_app.config.suppress_callback_exceptions = True
 
