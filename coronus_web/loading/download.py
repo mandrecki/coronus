e897076a7d7b08ea32bd706a844b00e310eb49e5
Marian Andrecki <marian.andrecki+eqt@gmail.com>
2020-03-22 16:57:53 +0000

    new, more general data loaded (split by regions, countries available)

---
+++ coronus_web/loading/download.py
@@ -0,0 +1,86 @@
+
+import numpy as np
+import pandas as pd
+
+
+def load_region_values(url):
+#     s = requests.get(url).content
+#     df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    df = pd.read_csv(url)
+    df = df.drop(columns=["Lat", "Long"])
+    df.loc[df["Province/State"].isna(), "Province/State"] = df.loc[df["Province/State"].isna(), "Country/Region"]
+    df = df.drop(columns=["Country/Region"])
+    df = df.set_index("Province/State").T
+    df = df.reset_index(drop=False).reset_index(drop=True)
+    df = df.rename(columns={"index": "Date"})
+    df.Date = pd.to_datetime(df.Date)
+    df = df.set_index("Date")
+    return df
+
+
+def load_country_values(url):
+    s = requests.get(url).content
+    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    df = df.drop(columns=["Lat", "Long", "Province/State"])
+    df = df.groupby("Country/Region").sum()
+    df = df.T
+    df = df.reset_index(drop=False).reset_index(drop=True)
+    df = df.rename(columns={"index": "Date"})
+    df.Date = pd.to_datetime(df.Date)
+    df = df.set_index("Date")
+    return df
+
+
+urls = dict(
+    confirmed="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv",
+    recovered="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv",
+    dead="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv",
+)
+
+GEOLEVELS = [
+    "State",
+    "Country",
+    # "Continent"
+    "Global"
+]
+
+
+def get_raw_df(url):
+    #     s = requests.get(url).content
+    #     df = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    df = pd.read_csv(url)
+    df = df.rename(columns={
+        "Province/State": "State",
+        "Country/Region": "Country",
+    })
+    df["Global"] = "Global"
+    df.loc[df["State"].isna(), "State"] = df.loc[df["State"].isna(), "Country"]
+    return df
+
+
+def to_spacetime(df, geolevel: str):
+    drop_cols = GEOLEVELS + ["Lat", "Long"]
+    drop_cols.remove(geolevel)
+    df = df.drop(columns=drop_cols)
+    df = df.groupby(geolevel).sum()
+    df = df.T
+    df.index.name = "Date"
+    df.index = pd.to_datetime(df.index)
+    return df
+
+
+def calculate_active(spacetime_dict):
+    active = spacetime_dict["confirmed"] - spacetime_dict["recovered"] - spacetime_dict["dead"]
+    return active.replace(0, np.nan)
+
+
+def get_frames():
+    raw = {count_type: get_raw_df(url) for count_type, url in urls.items()}
+    geography = raw["confirmed"][["State", "Country", "Global",  "Lat", "Long"]]
+    cases_by_geolevel = dict()
+    for geolevel in GEOLEVELS:
+        spacetime = {count_type: to_spacetime(raw[count_type], geolevel=geolevel) for count_type in raw.keys()}
+        spacetime["active"] = calculate_active(spacetime)
+        cases_by_geolevel[geolevel] = spacetime
+
+    return cases_by_geolevel, geography
--- coronus_web/loading/frames.py
+++ coronus_web/loading/frames.py
@@ -1,41 +1,21 @@
 import pandas as pd
-import numpy as np
-import io
-import requests
 
-def load_region_values(url):
-    s = requests.get(url).content
-    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
-    df = df.drop(columns=["Lat", "Long"])
-    df.loc[df["Province/State"].isna(), "Province/State"] = df.loc[df["Province/State"].isna(), "Country/Region"]
-    df = df.drop(columns=["Country/Region"])
-    df = df.set_index("Province/State").T
-    df = df.reset_index(drop=False).reset_index(drop=True)
-    df = df.rename(columns={"index": "Date"})
-    df.Date = pd.to_datetime(df.Date)
-    df = df.set_index("Date")
-    return df
+from .download import get_frames
 
-def load_country_values(url):
-    s = requests.get(url).content
-    df = pd.read_csv(io.StringIO(s.decode('utf-8')))
-    df = df.drop(columns=["Lat", "Long", "Province/State"])
-    df = df.groupby("Country/Region").sum()
-    df = df.T
-    df = df.reset_index(drop=False).reset_index(drop=True)
-    df = df.rename(columns={"index": "Date"})
-    df.Date = pd.to_datetime(df.Date)
-    df = df.set_index("Date")
-    return df
 
-url_reco = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
-url_dead = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
-url_conf = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
-df_reco = load_country_values(url_reco)
-df_conf = load_country_values(url_conf)
-df_dead = load_country_values(url_dead)
-df_active = df_conf - df_reco - df_dead
-df_active = df_active.replace(0, np.nan)
+cases_by_geolevel_type, geography = get_frames()
+
+
+def get_cases(geolevel: str, cases_type: str):
+    return cases_by_geolevel_type[geolevel][cases_type]
+
+
+default_geolevel = "Country"
+df_reco = get_cases(default_geolevel, "recovered")
+df_conf = get_cases(default_geolevel, "confirmed")
+df_dead = get_cases(default_geolevel, "dead")
+df_active = get_cases(default_geolevel, "active")
+
 df_aggregations = pd.concat([
     df_active.sum(axis=1).rename("Active cases"),
     df_conf.sum(axis=1).rename("Total cases"),
