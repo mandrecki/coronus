b65d1930325417dc083f6919d5ffecd2a067e89d
Marian Andrecki <marian.andrecki+eqt@gmail.com>
2020-03-24 07:42:34 +0000

    Colors on the map are now #number of cases

--- coronus_web/plotting/plots.py
+++ coronus_web/plotting/plots.py
@@ -42,3 +42,12 @@ def plot_interactive_df(df, ylabel, legend_label, sort_by=None, color_map={}):
     )
 
     return fig
+
+
+def human_format(num):
+    num = float('{:.3g}'.format(num))
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
